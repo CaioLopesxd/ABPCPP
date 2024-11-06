886edb8a2734d08b59dca9eef3473ec58e8024a0
caiolopesxd <r9506505@gmail.com>
2024-11-06 19:53:55 -0300

    inicio

---
+++ template_projeto_final.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ItemBiblioteca {
+protected:
+    int id, ano;
+    string titulo;
+    bool emprestado;
+
+public:
+	ItemBiblioteca(int id, string titulo, int ano);
+	
+    virtual void exibirDetalhes();
+    int getId();
+    bool isEmprestado();
+    void emprestar();
+    void devolver();
+};
+
+class Livro : public ItemBiblioteca {
+protected:
+	string autor;
+	string genero;
+	
+public:
+    Livro(int id, string titulo, int ano, string autor, string genero);      
+    void exibirDetalhes() override;
+};
+
+class Usuario {
+protected:
+    int id, limiteEmprestimos, emprestimosAtuais;
+    string nome;
+
+public:
+    Usuario(int id, string nome, int limiteEmprestimos);
+        
+    virtual void exibirUsuario();
+
+    bool podeEmprestar();
+    void realizarEmprestimo();
+    void realizarDevolucao();
+    int getId();
+};
+
+class Aluno : public Usuario {
+public:
+    Aluno(int id, string nome);
+};
+
+class Professor : public Usuario {
+public:
+    Professor(int id, string nome);
+};
+
+class Biblioteca {
+    vector<Livro> livros;
+    vector<Aluno> alunos;
+    vector<Professor> professores;
+
+public:
+    void adicionarLivro();
+    void listarLivros();
+    void adicionarUsuario();
+    void listarUsuarios();
+    void emprestarLivro();
+    void devolverLivro();
+    void menu();
+};
+
+int main() {
+	
+    return 0;
+}
+
+void Biblioteca::menu(){
+    cout << "1 - Adicionar Livro" << endl;
+    cout << "2 - Listar Livros" << endl;
+    cout << "3 - Adicionar Usuario" << endl;
+    cout << "4 - Listar Usuarios" << endl;
+    cout << "5 - Emprestar Livro" << endl;
+    cout << "6 - Devolver Livro" << endl;
+    cout << "0 - Sair" << endl;
+
+};
+
