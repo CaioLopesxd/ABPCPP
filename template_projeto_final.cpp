0fd336698457ef235997980f74275d3e550660b5
CaioLopes18 <r9506505@gmail.com>
2024-11-13 19:41:27 -0300

    começo dos metodos

--- template_projeto_final.cpp
+++ template_projeto_final.cpp
@@ -10,13 +10,39 @@ protected:
     bool emprestado;
 
 public:
-	ItemBiblioteca(int id, string titulo, int ano);
+	ItemBiblioteca(int id, string titulo, int ano) : id(id), titulo(titulo), ano(ano), emprestado(false) {};
 	
-    virtual void exibirDetalhes();
-    int getId();
-    bool isEmprestado();
-    void emprestar();
-    void devolver();
+    virtual void exibirDetalhes(){
+        cout << "ID: " << id << endl;
+        cout << "Titulo: " << titulo << endl;
+        cout << "Ano: " << ano << endl;
+        cout << "Emprestado: " << emprestado << endl;
+    };
+    int getId(){
+        return id;
+    };
+    bool isEmprestado(){
+        if(!emprestado){
+            return false;
+        }
+        return true;
+    };
+    void emprestar(){
+        if(emprestado){
+            cout << "Item ja emprestado" << endl;
+        }else{
+            cout << "Item" << titulo << "emprestado" << endl;
+            emprestado = true;
+        }
+    };
+    void devolver(){
+        if(emprestado){
+            cout << "Item" << titulo << "devolvido" << endl;
+            emprestado = false;
+        }else{
+            cout << "Item ja devolvido" << endl;
+        }
+    };
 };
 
 class Livro : public ItemBiblioteca {
@@ -25,8 +51,15 @@ protected:
 	string genero;
 	
 public:
-    Livro(int id, string titulo, int ano, string autor, string genero);      
-    void exibirDetalhes() override;
+    Livro(int id, string titulo, int ano, string autor, string genero) : ItemBiblioteca(id, titulo, ano), autor(autor), genero(genero) {};      
+    void exibirDetalhes() override{
+        cout << "ID: " << id << endl;
+        cout << "Titulo: " << titulo << endl;
+        cout << "Ano: " << ano << endl;
+        cout << "Autor: " << autor << endl;
+        cout << "Genero: " << genero << endl;
+        cout << "Emprestado: " << emprestado << endl;
+    };
 };
 
 class Usuario {
@@ -35,24 +68,42 @@ protected:
     string nome;
 
 public:
-    Usuario(int id, string nome, int limiteEmprestimos);
+    Usuario(int id, string nome, int limiteEmprestimos) : id(id), nome(nome), limiteEmprestimos(limiteEmprestimos), emprestimosAtuais(0) {};
         
-    virtual void exibirUsuario();
+    virtual void exibirUsuario(){
+        cout << "ID: " << id << endl;
+        cout << "Nome: " << nome << endl;
+        cout << "Limite de Emprestimos: " << limiteEmprestimos << endl;
+        cout << "Emprestimos Atuais: " << emprestimosAtuais << endl;
+    };
 
-    bool podeEmprestar();
-    void realizarEmprestimo();
-    void realizarDevolucao();
-    int getId();
+    bool podeEmprestar(){
+        if(emprestimosAtuais < limiteEmprestimos){
+            return true;
+        }
+        return false;
+    };
+    void realizarEmprestimo(){
+        if(podeEmprestar()){
+            
+        }else{
+            cout << "Usuario atingiu o limite de emprestimos" << endl;
+        }
+    };
+    void realizarDevolucao(){};
+    int getId(){
+        return id;
+    };
 };
 
 class Aluno : public Usuario {
 public:
-    Aluno(int id, string nome);
+    Aluno(int id, string nome) : Usuario(id, nome, 3) {};
 };
 
 class Professor : public Usuario {
 public:
-    Professor(int id, string nome);
+    Professor(int id, string nome) : Usuario(id, nome, 5) {};
 };
 
 class Biblioteca {
@@ -61,13 +112,58 @@ class Biblioteca {
     vector<Professor> professores;
 
 public:
-    void adicionarLivro();
-    void listarLivros();
-    void adicionarUsuario();
-    void listarUsuarios();
-    void emprestarLivro();
-    void devolverLivro();
-    void menu();
+    void adicionarLivro(int id,string titulo,int ano,string autor,string genero){
+        Livro livro(id, titulo, ano, autor, genero);
+        livros.push_back(livro);
+    };
+    void listarLivros(){
+        for(auto livro : livros){
+            livro.exibirDetalhes();
+        }
+    };
+    
+    void adicionarUsuario(char tipo, int id, string nome){
+        if(tipo == 'A'){
+            Aluno aluno(id, nome);
+            alunos.push_back(aluno);
+        }else if(tipo == 'P'){
+            Professor professor(id, nome);
+            professores.push_back(professor);
+        }
+    };
+    void listarUsuarios(){
+        for(auto aluno : alunos){
+            aluno.exibirUsuario();
+        }
+        for(auto professor : professores){
+            professor.exibirUsuario();
+        }
+    };
+    void emprestarLivro(int idLivro, int idUsuario,char tipo){
+        if(tipo == 'A'){
+            alunos[idUsuario].realizarEmprestimo();
+        }else if(tipo == 'P'){
+            professores[idUsuario].realizarEmprestimo();
+        }
+        livros[idLivro].emprestar();
+    };
+    void devolverLivro(int idLivro, int idUsuario,char tipo){
+        if(tipo == 'A'){
+            alunos[idUsuario].realizarDevolucao();
+        }else if(tipo == 'P'){
+            professores[idUsuario].realizarDevolucao();
+        }
+        livros[idLivro].devolver();
+    };
+    void menu(){
+        cout << "1 - Adicionar Livro" << endl;
+        cout << "2 - Listar Livros" << endl;
+        cout << "3 - Adicionar Usuario" << endl;
+        cout << "4 - Listar Usuarios" << endl;
+        cout << "5 - Emprestar Livro" << endl;
+        cout << "6 - Devolver Livro" << endl;
+        cout << "0 - Sair" << endl;
+    };
 };
 
 int main() {
@@ -75,14 +171,4 @@ int main() {
     return 0;
 }
 
-void Biblioteca::menu(){
-    cout << "1 - Adicionar Livro" << endl;
-    cout << "2 - Listar Livros" << endl;
-    cout << "3 - Adicionar Usuario" << endl;
-    cout << "4 - Listar Usuarios" << endl;
-    cout << "5 - Emprestar Livro" << endl;
-    cout << "6 - Devolver Livro" << endl;
-    cout << "0 - Sair" << endl;
-
-};
 
